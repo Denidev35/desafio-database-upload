4b313f49388a25eef017c7770330734ef5226ec2
Denilson Silva <denilson20@hotmail.com>
2020-04-18 18:59:48 -0300

    desafio finalizado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,73 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
-import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
+
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+interface Response {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Response> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    if (
+      !(await categoryRepository.findOne({
+        where: { title: category },
+      }))
+    ) {
+      const categoryTab = categoryRepository.create({
+        title: category,
+      });
+      await categoryRepository.save(categoryTab);
+    }
+
+    const categoryTitle = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError('value exceeded the total');
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryTitle?.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return {
+      id: transaction.id,
+      title,
+      value,
+      type,
+      category,
+    };
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,48 @@
-import Transaction from '../models/Transaction';
+import path from 'path';
+import csv from 'csvtojson';
+import fs from 'fs';
+
+import uploadConfig from '../config/uplaod';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  transactionFilename: string;
+}
+
+interface Transaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ transactionFilename }: Request): Promise<Transaction[]> {
+    const createTransactions = new CreateTransactionService();
+
+    const transactionFilePath = path.join(
+      uploadConfig.directory,
+      transactionFilename,
+    );
+
+    const transactions: Transaction[] = await csv().fromFile(
+      transactionFilePath,
+    );
+
+    for (const transaction of transactions) {
+      const { title, value, type, category } = transaction;
+
+      await createTransactions.execute({
+        title,
+        value,
+        type,
+        category,
+      });
+    }
+
+    await fs.promises.unlink(transactionFilePath);
+
+    return transactions;
   }
 }
 
